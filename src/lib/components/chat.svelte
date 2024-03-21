6a03fe08f9a54934b3e64d0844dc39339989c90d
umgefahren <55623006+umgefahren@users.noreply.github.com>
2024-03-21 12:01:28 +0100

    Add a generative ai chat box thingy

--- src/lib/components/chat.svelte
+++ src/lib/components/chat.svelte
@@ -1,58 +1,34 @@
 <script lang="ts">
-  import ChatMessageBubble from '$lib/components/chat_message.svelte';
+	import ChatMessageBubble from '$lib/components/chat_message.svelte';
 	import ChatTextbox from '$lib/components/chat_textbox.svelte';
-  import { createEventDispatcher } from 'svelte';
+	import { useChat } from 'ai/svelte';
 
-  let chat: HTMLDivElement;
+	const { input, handleSubmit, messages } = useChat({
+    api: '/app/assistant',
+		initialMessages: [
+			{
+				id: '1',
+				role: 'assistant',
+				content: 'Hi!'
+			},
+			{
+				id: '2',
+				role: 'assistant',
+				content: 'How can I help you today?'
+			}
+		]
+	});
 
-  let messages: ChatMessage[] = [];
-
-  let dispatch = createEventDispatcher();
-
-  /**
-   * Add a message to the chat
-   * @param message The ChatMessage object to add
-   */
-  export function addMessage(message: ChatMessage) {
-    messages = [...messages, message];
-  }
-
-  /**
-   * Add a bot message to the chat
-   * (type = bot; side = left)
-   * @param content The text of the message
-   */
-  export function addBotMessage(content: string) {
-    addMessage({ kind: 'bot', side: 'left', content });
-  }
-
-  /**
-   * Add a user message to the chat
-   * (type = user; side = right)
-   * @param content The text of the message
-   */
-  export function addUserMessage(content: string) {
-    addMessage({ kind: 'user', side: 'right', content });
-  }
-
-  function handleMessage(e: CustomEvent<{ content: string }>) {
-    let { content } = e.detail;
-
-    addUserMessage(content);
-    setTimeout(() => chat.scroll({ top: chat.scrollHeight, behavior: 'smooth' }), 0);
-    dispatch('message', { content });
-  }
 </script>
 
+<div class="flex h-full flex-col">
+	<div class="flex-1 overflow-auto">
+		{#each $messages as message}
+			<ChatMessageBubble {message} />
+		{/each}
+	</div>
 
-<div class="flex flex-col h-full">
-  <div bind:this={chat} class="flex-1 overflow-auto">
-    {#each messages as message (message)}
-      <ChatMessageBubble {message} />
-    {/each}
-  </div>
-
-  <div class="pt-2 border-t border-t-gray-300">
-    <ChatTextbox on:message={handleMessage}/>
-  </div>
+	<div class="border-t border-t-gray-300 pt-2">
+		<ChatTextbox {handleSubmit} input={input} />
+	</div>
 </div>
--- src/lib/components/chat_message.svelte
+++ src/lib/components/chat_message.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
-  import { FaceExplodeOutline, UserCircleSolid } from 'flowbite-svelte-icons';
-  export let message: ChatMessage;
+	import type { Message } from 'ai';
+	import { FaceExplodeOutline, UserCircleSolid } from 'flowbite-svelte-icons';
 
-  let name = message.kind == 'user' ? 'You' : 'Lina';
-  let dir = message.side == 'left' ? 'ltr' : 'rtl';
+	export let message: Message & {
+		side?: 'left' | 'right';
+	};
+
+	let name = message.role == 'user' ? 'You' : 'Lina';
+	message.side = message.role == 'user' ? 'right' : 'left';
+	let dir = message.role == 'assistant' ? 'ltr' : 'rtl';
 </script>
 
-<div class="flex items-start gap-2.5 mb-4" dir={dir}>
-  {#if message.kind === 'user'}
-    <UserCircleSolid class="w-5 h-5 mb-1" />
-  {:else}
-    <FaceExplodeOutline class="w-5 h-5 mb-1" />
-  {/if}
-   <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-      <div class="flex items-center space-x-2 rtl:space-x-reverse">
-         <span class="text-sm font-semibold text-gray-900 dark:text-white">{name}</span>
-      </div>
-      <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white text-{message.side} break-words" dir="ltr">{message.content}</p>
-   </div>
+<div class="mb-4 flex items-start gap-2.5" {dir}>
+	{#if message.role === 'user'}
+		<UserCircleSolid class="mb-1 h-5 w-5" />
+	{:else}
+		<FaceExplodeOutline class="mb-1 h-5 w-5" />
+	{/if}
+	<div
+		class="leading-1.5 flex w-full max-w-[320px] flex-col rounded-e-xl rounded-es-xl border-gray-200 bg-gray-100 p-4 dark:bg-gray-700"
+	>
+		<div class="flex items-center space-x-2 rtl:space-x-reverse">
+			<span class="text-sm font-semibold text-gray-900 dark:text-white">{name}</span>
+		</div>
+		<p
+			class="py-2.5 text-sm font-normal text-gray-900 dark:text-white text-{message.side} break-words"
+			dir="ltr"
+		>
+			{message.content}
+		</p>
+	</div>
 </div>
-
