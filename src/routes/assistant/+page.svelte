96ced5c1ebfd4a7ed59c22ccca7cf7fbcfe3fc4e
vypxl <thomas@vypxl.io>
2024-03-21 01:02:18 +0100

    assistant route base

--- src/routes/assistant/+page.svelte
+++ src/routes/assistant/+page.svelte
@@ -1,6 +1,15 @@
-<script>
-  import { BottomNav, BottomNavItem, Tooltip, Skeleton, ImagePlaceholder } from 'flowbite-svelte';
+<script lang="ts">
+  import Chat from '$lib/components/chat.svelte';
+
+  import { onMount } from 'svelte';
+
+  let chat: Chat;
+
+  onMount(() => {
+    chat.addBotMessage('Hi!');
+    chat.addBotMessage('Ask me about your portfolio, about funds, the app, investing tipps, ...')
+  })
+
 </script>
 
-<Skeleton class="py-4" />
-<ImagePlaceholder class="pb-20" />
+<Chat bind:this={chat} />
