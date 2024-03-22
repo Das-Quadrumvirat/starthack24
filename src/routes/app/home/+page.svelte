547ee38fe13fd392dcbef90a02a2bab2f78597d4
TecTrixer <tonihoevedes@gmail.com>
2024-03-22 02:16:14 +0100

    initial scoreslider

--- src/routes/app/home/+page.svelte
+++ src/routes/app/home/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import PriceChart from '$lib/components/price_chart.svelte';
+  import ScoreSlider from '$lib/components/ScoreSlider.svelte';
 
   let portfolio = [
     { isin: 'AT0000784756', weight: 1 },
@@ -10,5 +11,8 @@
 </script>
 
 <div class="pb-20">
+  <ScoreSlider value={0.2} name={"Social Score"}/>
+  <ScoreSlider value={0.7} name={"Environmental Score"}/>
+  <ScoreSlider value={1.0} name={"Sustainability Score"}/>
   <PriceChart title="Your Balance" tooltip="Balance" isins={portfolio} />
 </div>
